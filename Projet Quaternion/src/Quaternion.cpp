2e76b155de1c0ade85dab220f629f974ca4af44e
Piccolo <nelsonsulter91@gmail.com>
2019-05-26 16:42:00 +0200

    SommeWIP

--- Projet Quaternion/src/Quaternion.cpp
+++ Projet Quaternion/src/Quaternion.cpp
@@ -9,3 +9,72 @@ Quaternion::~Quaternion()
 {
     //dtor
 }
+
+
+Quaternion::SetX(float MaValeur){
+
+    x = MaValeur;
+
+}
+
+
+Quaternion::SetY(float MaValeur){
+
+    y = MaValeur;
+
+}
+
+Quaternion::SetZ(float MaValeur){
+
+    z = MaValeur;
+
+}
+
+Quaternion::SetW(float MaValeur){
+
+    w = MaValeur;
+
+}
+
+Quaternion::GetX(float MaValeur){
+
+    return x;
+
+}
+
+Quaternion::GetY(float MaValeur){
+
+    return y;
+
+}
+
+Quaternion::GetZ(float MaValeur){
+
+    return z;
+
+}
+
+Quaternion::GetW(float MaValeur){
+
+    return w;
+
+}
+
+
+Quaternion somme(Quaternion q, Quaternion q2) {
+
+    float newX;
+    float newY;
+    float newZ;
+    float newW;
+
+    newX = q.GetX() + q2.GetX();
+    newY = q.GetY() + q2.GetY();
+    newZ = q.GetZ() + q2.GetZ();
+    newW = q.GetW() + q2.GetW();
+
+    return new Quaternion();
+
+
+
+}
