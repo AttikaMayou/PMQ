466e06bdf71392a415356b0bd1e10d51f9a7a16f
Régis <regis.poizat@cegetel.net>
2019-05-26 14:47:01 +0200

    classe quaternion

---
+++ Projet Quaternion/include/Quaternion.h
@@ -0,0 +1,18 @@
+#ifndef QUATERNION_H
+#define QUATERNION_H
+
+
+class Quaternion
+{
+    public:
+        Quaternion();
+        virtual ~Quaternion();
+    protected:
+    private:
+        float x;
+        float y;
+        float z;
+        float w;
+};
+
+#endif // QUATERNION_H
