10a8b3b218f27f3f5bee702c962d1297034aa700
Piccolo <nelsonsulter91@gmail.com>
2019-05-31 10:45:38 +0200

    premier commit

--- Projet Quaternion/main.cpp
+++ Projet Quaternion/main.cpp
@@ -3,19 +3,20 @@
 #include <GL/gl.h>
 #include <GL/glu.h>
 
-#include "glut.h"
-#include "SOIL.h"
+//#include "glut.h"
+//#include "SOIL.h"
 
-#include "Camera.h"
-#include "Map.h"
+//#include "Camera.h"
+//#include "Map.h"
 
+//#include ""
 using namespace std;
 
 int main()
 {
-    Quaternion test = new Quaternion(1,1,1,1);
+    Quaternion test = new Quaternion(1.0,1.0,1.0,1.0);
 
-    Quaternion test2 = new Quaternion(2,2,2,2);
+    Quaternion test2 = new Quaternion(2.0,2.0,2.0,2.0);
 
     Quaternion test3 = test.somme(test2);
 
